5937de74cee6ca13c294f8b796635d97d023d097
Magician774 <hodjicsuper22@gmail.com>
2024-08-02 11:19:59 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,347 @@
+//function hello(a , b  ) {
+//console.log(a * b );
+//}
+//hello (2, 2)
+
+
+//const fn = function (a , b) {
+//console.log(a / b);
+//}
+//fn (8, 4)
+
+//const plus = (a , b) => {
+//    console.log(a + b)}
+//plus (16 , 16)
+
+//const minus = (a , b) => a-b;
+//console.log( minus (14 , 16));
+//const me = (name , surname , age) => {
+//console.log(name , surname , age);
+//}
+//me("khozhiakbar" , "mullabaev" , "14")
+//const fn2 = (a) => {
+   // if (a < 0) {
+      //  console.log("-");
+  //  }
+
+   // if (a == 2) {
+     //   console.log("2");
+   // } if (a == 0) {
+
+    //    console.log("0");
+   // }
+   // else if(a > 0){
+    //    console.log("+");
+    //}
+
+//}
+
+//fn2(prompt("NUmber"))
+const person = (nemr , surname , age  ) =>{
+
+    if (age > 18) {
+        console.log    (`hello ${nemr} ${surname}, im ${age} `);
+    }
+    else{
+        console.log("go home");
+    }
+}
+person("Khozhiakbar " , " Mullabaev" , 19)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
